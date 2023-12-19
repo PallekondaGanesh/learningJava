1bc4e41f34d9c33323ac1884cfa0bb7d62e9a503
PallekondaGanesh <pallekondaganesh143@gmail.com>
2023-12-19 12:26:23 +0530

    added README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# learning java
+
+
+## my learning source
+ - youtube video : telsko channel 2023 java video
+ - tools         : vscode
+
+
+
+## topics
+ - basic data types : learned : 19-12-2023
--- basicDatatypes/BasicDatatypes.java
+++ basicDatatypes/BasicDatatypes.java
@@ -1,3 +1,4 @@
+package basicDatatypes;
 
 class BasicDatatypes{
  public static void main(String args[]){
@@ -7,4 +8,13 @@ class BasicDatatypes{
     float  isFloat = 20.000f;
     System.out.print(x + y + "" + isTrue + isFloat);
  }
+}
+
+class pakodi {
+   public int y;
+   public int x;
+   pakodi(int x,int y){
+      this.x=x;
+      this.y=y;
+   }
 }
