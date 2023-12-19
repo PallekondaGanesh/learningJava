74860396378238642a00349a74ed7ab1207178b8
PallekondaGanesh <pallekondaganesh143@gmail.com>
2023-12-19 12:13:21 +0530

    basic data types learned

---
+++ basicDatatypes/BasicDatatypes.java
@@ -0,0 +1,10 @@
+
+class BasicDatatypes{
+ public static void main(String args[]){
+    int x=10;
+    String y = "hello World";
+    Boolean isTrue = true;
+    float  isFloat = 20.000f;
+    System.out.print(x + y + "" + isTrue + isFloat);
+ }
+}
